741ab59bc3a798a538de7b05c8c053ef2016711e
a <a>
2016-07-07 13:57:07 +0800

    Using dynamic method to get all module list then append __all__ list.

--- CrawScripts/__init__.py
+++ CrawScripts/__init__.py
@@ -1,15 +1,20 @@
-__all__ = ['bomb01', 
-           'teepr',
-           'ptt01cc',
-           'pixnet',
-           'toments',
-           'daliulian',
-           'buzzlife',
-           'buzzhand',
-           'gjoyz',
-           'fooding',
+from os.path import dirname, basename, isfile
+import glob
 
-           
-           
-           
-          ]
+
+
+
+
+
+
+
+except_file_name_list = ['__init__.py', 'auto_post.py', 'base_craw.py']
+modules = glob.glob(dirname(__file__)+"/*.py")
+
+__all__ = list()
+for f in modules:
+  if isfile(f):
+    file_name = basename(f) 
+    if file_name not in except_file_name_list:
+      __all__.append(file_name[:-3])
+
